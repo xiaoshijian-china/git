e183bcc5b31a40f8a12e2d163059de6258abfadf
Sky <2946252075@qq.com>
2022-03-07 19:35:50 +0800

    second commit

--- src/main/java/com/git/Gittest.java
+++ src/main/java/com/git/Gittest.java
@@ -2,7 +2,7 @@ package com.git;
 
 public class Gittest {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello git");
 
     }
 }
